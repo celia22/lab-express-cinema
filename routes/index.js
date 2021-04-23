e62fca3b6a6503c9637e98d3a6fe75bb7b6347ca
celia22 <celialopez22@gmail.com>
2021-04-23 12:32:44 +0200

    finish lab

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/Movie.model.js');
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
+router.get('/movies', (req, res, next) => {
+  Movie.find()
+    .then(allMoviesFromDB => {
+     console.log('Retrieved movies from DB:', allMoviesFromDB);
+      res.render('movies', { movies: allMoviesFromDB });
+    })
+    .catch(error => {
+      console.log('Error while getting the movies from the DB: ', error);
+      next(error);
+    });
+});
+
+router.get('/movies/:id', (req, res, next) => {
+  const { id } = req.params;
+  Movie.findById(id)
+    .then(movie => res.render('details', movie))
+    .catch(error => {
+      console.log('Error while retrieving movie details: ', error);
+      next(error);
+    });
+});
+
 module.exports = router;
+
